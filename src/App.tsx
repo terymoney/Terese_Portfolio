37fdbc67185a328b07e535025436aba9764ee8b8
Tery Money <terymoney@gmail.com>
2026-02-02 11:47:53 +0100

    remove tlinkpay routes/imports

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,12 @@ import Web3 from "./pages/Web3";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "@/components/ScrollToTop";
 
-// ✅ T-Link Pay pages
-import TLinkPayHome from "./pages/tlinkpay/TLinkPayHome";
-import TLinkPayInvoices from "./pages/tlinkpay/TLinkPayInvoices";
-import TLinkPayInvoiceDetail from "./pages/tlinkpay/TLinkPayInvoiceDetail";
-import TLinkPayPay from "./pages/tlinkpay/TLinkPayPay";
 
 const App = () => (
   <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
@@ -30,13 +24,6 @@ const App = () => (
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:slug" element={<ProjectDetail />} />
           <Route path="/web3" element={<Web3 />} />
-
-          {/* ✅ T-Link Pay routes */}
-          <Route path="/t-link-pay" element={<TLinkPayHome />} />
-          <Route path="/t-link-pay/invoices" element={<TLinkPayInvoices />} />
-          <Route path="/t-link-pay/invoices/:invoiceId" element={<TLinkPayInvoiceDetail />} />
-          <Route path="/t-link-pay/pay/:invoiceId" element={<TLinkPayPay />} />
-
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
